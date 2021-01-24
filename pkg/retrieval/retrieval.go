e0b8da50ae1945b477836ec8c3d3f52fec3b37b8
Metacertain <metacertain@gmail.com>
2021-01-24 18:48:04 +0100

    Save pricetable updates and test updates

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -230,7 +230,7 @@ func (s *Service) retrieveChunk(ctx context.Context, addr swarm.Address, sp *ski
 		}
 	}()
 
-	returnedTarget, returnedPrice, err := s.pricer.ReadPricingHeaders(returnedHeaders)
+	returnedTarget, returnedPrice, returnedIndex, err := s.pricer.ReadPricingResponseHeaders(returnedHeaders)
 	if err != nil {
 		return nil, peer, fmt.Errorf("retrieval headers: read returned: %w", err)
 	}
@@ -239,7 +239,7 @@ func (s *Service) retrieveChunk(ctx context.Context, addr swarm.Address, sp *ski
 	s.logger.Debugf("retrieval headers: original target %v with price as %v, from peer %s", addr, chunkPrice, peer)
 	// returned checker
 	if returnedPrice != chunkPrice {
-		// save priceHeaders["price"] corresponding row for peer
+		s.pricer.NotifyPeerPrice(peer, returnedPrice, returnedIndex) // save priceHeaders["price"] corresponding row for peer
 		chunkPrice = returnedPrice
 		//return nil, swarm.Address{}, fmt.Errorf("price mismatch: %w", err)
 	}
@@ -380,6 +380,7 @@ func (s *Service) handler(ctx context.Context, p p2p.Peer, stream p2p.Stream) (e
 	// to get price Read in headler,
 	returnedHeaders := stream.ResponseHeaders()
 	chunkPrice, err := s.pricer.ReadPriceHeader(returnedHeaders)
+
 	if err != nil {
 		// if not found in returned header, compute the price we charge for this chunk and
 		s.logger.Warningf("")
