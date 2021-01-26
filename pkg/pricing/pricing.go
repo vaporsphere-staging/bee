1dbdbeb58480b6c45f5763aefd98241b5435ce58
Metacertain <metacertain@gmail.com>
2021-01-26 19:05:49 +0100

    post rebase

--- pkg/pricing/pricing.go
+++ pkg/pricing/pricing.go
@@ -42,7 +42,6 @@ type PaymentThresholdObserver interface {
 }
 
 type Service struct {
-	overlay                  swarm.Address
 	streamer                 p2p.Streamer
 	logger                   logging.Logger
 	paymentThreshold         uint64
